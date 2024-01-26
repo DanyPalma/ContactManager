e22359fc9f4ffcf222ecbcab2abca616a67bd10a
Emily Adams <emilysgadams@gmail.com>
2024-01-26 17:09:50 -0500

    Create register.php

---
+++ api/register.php
@@ -0,0 +1,43 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$login = $inData["login"];
+	$password = $inData["password"];
+
+	$conn = new mysqli("localhost", "Group2API", "8123uasyewt2", "UserInfo"); 	
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Users (firstName,lastName,Login,Password) VALUES (?,?)");
+		$stmt->bind_param("ss", $firstName, $lastName, $login, $password);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	
+?>
