c64bb38e86c3d7c0cd4a22e998682cb512cb86ff
dany <danielpalmaromero@outlook.com>
2024-01-28 21:40:44 -0500

    Create delete.php

---
+++ api/delete.php
@@ -0,0 +1,43 @@
+<?php
+
+    $in_data = getRequestInfo();
+
+    $id = $in_data["ID"];
+
+    if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+        $stmt = $conn->prepare("DELETE FROM Contacts WHERE ID = ?");
+        $stmt->bind_param("s", $id);
+        $stmt->execute();
+        $stmt->close();
+        $conn->close();
+
+        returnWithError("")
+
+    }
+
+
+    $conn = new mysqli("localhost", "Group2API", "8123uasyewt2", "UserInfo"); 
+
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
+
+    function sendResultInfoAsJson( $obj )
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+
+    function returnWithError( $err )
+    {
+        $retValue = '{"error":"' . $err . '"}';
+        sendResultInfoAsJson( $retValue );
+    }
+
+?>
