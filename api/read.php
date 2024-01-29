62c6799cf8e047cf1fcae8c00f0caf1fb0b705b5
dany <danielpalmaromero@outlook.com>
2024-01-28 20:13:46 -0500

    Update read.php

--- api/read.php
+++ api/read.php
@@ -4,36 +4,38 @@
 
     $user_id = $in_data["UserID"];
 
-    $conn = new mysqli("localhost", "Group2API", "813uasyewt2", "UserInfo");
+    $conn = new mysqli("localhost", "Group2API", "8123uasyewt2", "UserInfo");
     if( $conn->connect_error)
     {
-        returnWithError ( $conn->connect_error );
+        returnWithError( $conn->connect_error);
     }
     else
     {
-        $stmt = $conn->prepare("SELECT * FROM Contacts");
+        $stmt = $conn->prepare("SELECT * FROM Contacts where User_ID = (?)");
+        $stmt->bind_param("i", $user_id);
         $stmt->execute();
         $stmt->close();
         $conn->close();
         returnWithError("");
     }
 
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
 
-    function getRequestInfo()
-    {
-        return json_decode(file_get_contents('php://input'), true);
-    }
-
-    function sendResultInfoAsJson( $obj )
-    {
-        header('Content-type: application/json');
-        echo $obj;
-    }
-
-    function returnWithError( $err )
-    {
-        $retValue = '{"error":"' . $err . '"}';
-        sendResultInfoAsJson( $retValue );
-    }
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	
 
 ?>
