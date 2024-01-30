5bd91100594211c18604657783f0a8ea0d00555e
dany <danielpalmaromero@outlook.com>
2024-01-30 14:35:55 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
 const urlBase = 'https://d4ny.tech/api';
 const extension = 'php';
 
-function login() {
+function login(event) {
+
+    event.preventDefault();
 
     let login = document.getElementById("logUsername").value;
     let password = document.getElementById("logPassword").value;
-    var hash = md5(password);
 
-    let tmp = {login:login,password:hash};
+    let tmp = {login:login,password:password};
 
     let payload = JSON.stringify(tmp);
 
@@ -19,6 +20,81 @@ function login() {
 
     xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 
-    
+    try
+    {
+        xhr.onreadystatechange = function()
+        {
+            if(this.readyState == 4 & this.status == 200)
+            {
+                let jsonObject = JSON.parse(xhr.responseText);
+                userID = jsonObject.id;
+                if(userID < 1)
+                {
+                    document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+                    return;
+                }
 
+                firstName = jsonObject.firstName;
+                lastName = jsonObject.lastName;
+
+                saveCookie();
+
+                console.log(firstName + " " + lastName + " " + userID);
+
+                window.location.href = "landing.html"
+            }
+        };
+        xhr.send(payload);
+    }   
+    catch(err)
+    {
+        document.getElementById("loginResult").innerHTML = err.message;
+    } 
+
+}
+
+function saveCookie()
+{
+    let minutes = 20;
+    let date = new Date();
+    date.setTime(date.getTime()+(minutes*60*1000));
+    document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userID=" + userID + ";expires=" + date.toGMTString();
+}
+
+function readCookie()
+{
+    userID = -1;
+    let data = document.cookie;
+    let splits = data.split(",");
+    for(var i = 0; i < splits.length; i++) {
+        let thisOne = splits[i].trim();
+        let tokens = thisOne.split("=");
+        if(tokens[0] == "firstName"){
+            firstName = tokens[1];
+        }
+        else if( tokens[0] == "lastName"){
+            lastName = tokens[1];
+        }
+        else if( tokens[0] == "userID"){
+            userID = parseInt(tokens[1].trim());
+        }
+    }
+
+    if(userID < 0) {
+        window.location.href="index.html";
+    }
+    else 
+    {
+        document.getElementById("userName").innerHTML = "Welcome " + firstName + " " + lastName + "!"; 
+    }
+
+}
+
+function doLogout()
+{
+    userId = 0;
+	firstName = "";
+	lastName = "";
+	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	window.location.href = "index.html";
 }
