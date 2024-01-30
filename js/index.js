e3fb624a8737a852b2554224ae4fa4f579641b83
dany <danielpalmaromero@outlook.com>
2024-01-30 14:40:48 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -58,7 +58,7 @@ function saveCookie()
     let minutes = 20;
     let date = new Date();
     date.setTime(date.getTime()+(minutes*60*1000));
-    document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userID=" + userID + ";expires=" + date.toGMTString();
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
 }
 
 function readCookie()
@@ -90,8 +90,9 @@ function readCookie()
 
 }
 
-function doLogout()
+function logout(event)
 {
+    event.preventDefault;
     userId = 0;
 	firstName = "";
 	lastName = "";
