2fa7fbf9f6e96d1e551b928302e24d26b92583ca
dany <71732131+DanyPalma@users.noreply.github.com>
2024-01-31 20:13:21 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -118,34 +118,34 @@ function saveCookie()
 	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
 }
 
-// function readCookie()
-// {
-//     userID = -1;
-//     let data = document.cookie;
-//     let splits = data.split(",");
-//     for(var i = 0; i < splits.length; i++) {
-//         let thisOne = splits[i].trim();
-//         let tokens = thisOne.split("=");
-//         if(tokens[0] == "firstName"){
-//             firstName = tokens[1];
-//         }
-//         else if( tokens[0] == "lastName"){
-//             lastName = tokens[1];
-//         }
-//         else if( tokens[0] == "userID"){
-//             userID = parseInt(tokens[1].trim());
-//         }
-//     }
+function readCookie()
+{
+    userID = -1;
+    let data = document.cookie;
+    let splits = data.split(",");
+    for(var i = 0; i < splits.length; i++) {
+        let thisOne = splits[i].trim();
+        let tokens = thisOne.split("=");
+        if(tokens[0] == "firstName"){
+            firstName = tokens[1];
+        }
+        else if( tokens[0] == "lastName"){
+            lastName = tokens[1];
+        }
+        else if( tokens[0] == "userID"){
+            userID = parseInt(tokens[1].trim());
+        }
+    }
 
-//     if(userID < 0) {
-//         window.location.href="index.html";
-//     }
-//     else 
-//     {
-//         document.getElementById("userName").innerHTML = "Welcome " + firstName + " " + lastName + "!"; 
-//     }
+    if(userID < 0) {
+        window.location.href="index.html";
+    }
+    else 
+    {
+        document.getElementById("userName").innerHTML = "Welcome " + firstName + " " + lastName + "!"; 
+    }
 
-// }
+}
 
 function doLogout(event)
 {
