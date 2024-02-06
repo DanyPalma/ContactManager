f4c0d38e10a6a8f8e8a5f77a98ca36d74ac5497f
dany <danielpalmaromero@outlook.com>
2024-02-06 01:10:46 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,45 +3,52 @@
 const urlBase = "/api";
 const extension = "php";
 
-function deleteContact(event) {
-  event.preventDefault();
+function deleteContact(button) {
+  
+  var innerDiv = button.parentNode;
 
-  let ID = event.target.parentNode.parentNode.querySelector(".idBox").textContent.split(":")[1].trim();
-  // text content doesnt exist though
+  var OuterDiv = innerDiv.parentNode;
 
-  let tmp = {
-    ID: ID,
-  };
+  var paragraph = OuterDiv.querySelector('p');
 
-  let payload = JSON.stringify(tmp);
+  var paragraphText = paragraph.textContent;
+  
 
-  let url = urlBase + "/delete." + extension;
+  console.log(paragraphText);
 
-  let xhr = new XMLHttpRequest();
+  // let tmp = {
+  //   ID: ID,
+  // };
 
-  xhr.open("POST", url, true);
+  // let payload = JSON.stringify(tmp);
 
-  xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+  // let url = urlBase + "/delete." + extension;
 
-  try {
-    xhr.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        let jsonObject = JSON.parse(xhr.responseText);
-        err = jsonObject.error;
-        if (err != "") {
-          alert("Error deleting contact, please try again");
-          return;
-        }
-      }
-    };
+  // let xhr = new XMLHttpRequest();
 
-    xhr.send(payload);
-  } catch (err) {
-    alert("Error: " + err.message);
-  }
+  // xhr.open("POST", url, true);
 
-  alert("Successfully deleted contact");
-  window.location.href = "./newlanding.html";
+  // xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+  // try {
+  //   xhr.onreadystatechange = function () {
+  //     if (this.readyState == 4 && this.status == 200) {
+  //       let jsonObject = JSON.parse(xhr.responseText);
+  //       err = jsonObject.error;
+  //       if (err != "") {
+  //         alert("Error deleting contact, please try again");
+  //         return;
+  //       }
+  //     }
+  //   };
+
+  //   xhr.send(payload);
+  // } catch (err) {
+  //   alert("Error: " + err.message);
+  // }
+
+  // alert("Successfully deleted contact");
+  // window.location.href = "./newlanding.html";
 }
 
 function addContact(event) {
@@ -224,13 +231,13 @@ function doSearch(event) {
 
           mainInfoElement.innerHTML += `
           <div class="contact-buttons">
-            <button onclick="editContact(event);">
+            <button onclick="editContact(this);">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="smaller-logo">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
               </svg>
             </button>
       
-            <button onclick="deleteContact(event);">
+            <button onclick="deleteContact(this);">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="smaller-logo">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
               </svg>
