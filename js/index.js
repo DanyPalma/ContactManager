8ab98a4d581a5c97cb142dee74e8e3d79b9f7213
dany <71732131+DanyPalma@users.noreply.github.com>
2024-02-05 23:06:51 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,12 +4,7 @@ const urlBase = "/api";
 const extension = "php";
 
 function deleteContact(button) {
-  let card = button.closest("invisible");
-
-  let ID = card.value;
-
-  console.log(ID);
-  // Find the closest sibling div with the class "invisible"
+  
 }
 
 function addContact(event) {
@@ -180,8 +175,8 @@ function doSearch(event) {
           cardContainer.appendChild(card);
 
           card.innerHTML += `
-          <div id="contact-button-div" class="contact-buttons">
-            <button onclick="editContact(this);">
+          <div class="contact-buttons">
+            <button id="contact-button" onclick="editContact(this);">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="smaller-logo">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
               </svg>
@@ -195,7 +190,7 @@ function doSearch(event) {
           </div>
         `;
 
-          let contactDiv = document.getElementById("contact-button-div");
+          let contactDiv = document.getElementById("contact-button");
 
           let contactID = document.createElement("h2");
           contactID.className = "invisible";
