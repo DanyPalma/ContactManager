e4d6f24d111a29bf3465b3a881115a05110e0b05
dany <71732131+DanyPalma@users.noreply.github.com>
2024-02-05 22:49:48 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,8 +6,6 @@ const extension = "php";
 function deleteContact(button) {
   var card = button.closest(".card");
 
-  var invisibleH2 = card.querySelector('h2[style="display: none;"]');
-
   // Find the closest sibling div with the class "invisible"
   console.log(invisibleH2.textContent);
 }
@@ -168,10 +166,6 @@ function doSearch(event) {
           let emailElement = document.createElement("p");
           emailElement.textContent = "Email: " + cardData.Email;
 
-          let contactID = document.createElement("h2");
-          contactID.className = "invisible";
-          contactID.textContent = cardData.ID;
-
           contactInfoElement.append(contactID);
 
           // Append phone and email elements to contact information div
@@ -186,7 +180,7 @@ function doSearch(event) {
           cardContainer.appendChild(card);
 
           card.innerHTML += `
-          <div class="contact-buttons">
+          <div id="contact-button-div" class="contact-buttons">
             <button onclick="editContact(this);">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="smaller-logo">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
@@ -200,6 +194,14 @@ function doSearch(event) {
             </button >
           </div>
         `;
+        let contactID = document.createElement("h2");
+        contactID.className = "invisible";
+        contactID.textContent = cardData.ID;
+
+        let contactDiv = document.getElementById("contact-button-div");
+
+        contactDiv.appendChild("contactID");
+
         }
       }
     };
