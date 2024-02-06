2ea011fec4c9009a091f898b8c4fe6b50da94018
dany <71732131+DanyPalma@users.noreply.github.com>
2024-02-05 23:52:52 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,10 +3,22 @@
 const urlBase = "/api";
 const extension = "php";
 
-function deleteContact(button) {
-  let cardDiv = button.closest(".idBox").innerHTML;
+function deleteContact(event) {
+  event.preventDefault();
 
-  console.log(cardDiv);
+  var cards = document.querySelectorAll(".card");
+
+  cards.forEach(function (card) {
+    var button = card.querySelector(".log-button");
+    var h2 = card.querySelector("h2");
+
+    button.addEventListener("click", function () {
+      // Log the content of the corresponding H2
+      console.log(h2.textContent);
+    });
+
+    
+  });
 }
 
 function addContact(event) {
@@ -159,7 +171,7 @@ function doSearch(event) {
 
           // create p element for ID
           let idElement = document.createElement("p");
-          idElement.className = "idBox"
+          idElement.className = "idBox";
           idElement.textContent = "ID: " + cardData.ID;
 
           // Create div element for contact information
@@ -189,13 +201,13 @@ function doSearch(event) {
 
           card.innerHTML += `
           <div class="contact-buttons">
-            <button onclick="editContact(this);">
+            <button onclick="editContact(event);">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="smaller-logo">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
               </svg>
             </button>
       
-            <button onclick="deleteContact(this);">
+            <button onclick="deleteContact(event);">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="smaller-logo">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
               </svg>
